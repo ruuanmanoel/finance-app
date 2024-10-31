2e6851d1e77d4e83ca1fc3fa7343b9f43fe8bb16
Ruan Manoel <ruuanmanoel@gmail.com>
2024-10-30 21:53:45 -0300

    ✨ feat: criação das transações dinamicamente

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -3,7 +3,8 @@ import 'package:flutter/material.dart';
 class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
-  TransactionForm({super.key});
+  final void Function(String, double) onSubmit;
+  TransactionForm(this.onSubmit, {super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -30,8 +31,9 @@ class TransactionForm extends StatelessWidget {
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+                    onSubmit(title, valu
                   },
                   child: const Text(
                     'Nova transação',
